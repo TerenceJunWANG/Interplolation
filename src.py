5032ff1c0f34129cfc833fe7a254c57d344d1284
TerenceJunWANG <wjzhy0717@gmail.com>
2017-03-31 19:20:29 +0800

    change the save method to hdfs lib

--- src.py
+++ src.py
@@ -3,12 +3,12 @@ import pandas as pd
 import pywt as wt
 
 from scipy.interpolate import interp1d
-import matplotlib.pyplot as plt
 
 import datetime
 
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
+from pyspark.sql.types import Row
 
 from hdfs import *
 
@@ -147,37 +147,34 @@ def floatrange(start, stop, steps):
     '''
     return [start + float(i) * (stop - start) / (float(steps) - 1) for i in range(steps)]
 
+def Interpolation():
 
-if __name__ == "__main__":
-    startTimeMain = datetime.datetime.now()
 
-    RRInfoFilePath = '/home/terence01/workspace/Spark/Interpolatation/RRInfo/'
-    txtFilePath = '/home/terence01/workspace/Spark/Interpolatation/TextData/'
-    HDFSPath = '/Intropolation'
+    HDFSPath = '/Interpolation'
     HDFSURL = 'http://Master.Hadoop:50070'
-    OutPutPath = 'hdfs://Master.Hadoop:9000/Intropolation/'
-
-    #sc = SparkContext(appName="Interpolation")
-    conf = SparkConf().setAppName('Interpolation')
-    #conf.setMaster("spark://Master.Hadoop:7077")
-    conf.set("spark.executor.heartbeatInterval", "60s")
-    sc = SparkContext(conf = conf)
 
     client = Client(HDFSURL, root= '/')
     client.delete(HDFSPath, True)
 
 
-    print "<----> Interpolation Main begin\n"
+
+    startTimeMain = datetime.datetime.now()
+
+    RRInfoFilePath = '/home/terence01/workspace/Spark/Interpolation/RRInfo/'
+    txtFilePath = '/home/terence01/workspace/Spark/Interpolation/TextData/'
+
+    OutPutPath = 'hdfs://Master.Hadoop:9000/Interpolation/'
+
+    icounter = 0
 
     for icounter in range(len(ALL_GroupFiles)):
 
-        # DATAFILE    = PATH + "\\" + strcat(str(ALL_GroupFiles(icounter)),'_BeatType.txt')
         FileName = str(ALL_GroupFiles[icounter])
         BeatTypeFile = RRInfoFilePath + FileName + "_BeatType.txt"
         VoltageFile = txtFilePath + FileName + ".txt"
         startTimeEachFile = datetime.datetime.now()
 
-        print "<----> Interpolation Main Processing %s begin\n"%(VoltageFile)
+        print "<----> Interpolation Main Processing %s begin\n" % (VoltageFile)
 
         print "<----> Interpolation Main load dataframe for beatType\n"
         dataFrame = pd.read_csv(BeatTypeFile, delim_whitespace=True)
@@ -196,11 +193,10 @@ if __name__ == "__main__":
 
         ## need to transfer to spark
         dataFrame['RR_interval'] = dataFrame['RR_interval'] + 1
-        RR_interval = dataFrame.loc[:,'RR_interval'].tolist()
+        RR_interval = dataFrame.loc[:, 'RR_interval'].tolist()
         del (RR_interval[0])
 
         print "<----> Interpolation Main MaxLength = 2114\n"
-        # MaxLength = max(RR_interval)
         MaxLength = 2114
 
         #    #remove the first one and the last one
@@ -236,12 +232,12 @@ if __name__ == "__main__":
 
             Denosied_Signal = wt.waverec((cNA8, cND8, cD7, cD6, cD5, cD4, cD3, cD2, cD1), 'sym5')
             endTimeDenosing = datetime.datetime.now()
-            print "<----> Interpolation Main Denosing for %s end, execute time %ds\n" % (columnName, (endTimeDenosing-startTimeDenosing).seconds)
+            print "<----> Interpolation Main Denosing for %s end, execute time %ds\n" % (columnName, (endTimeDenosing - startTimeDenosing).seconds)
             # End Denosing
 
             # Interpolation
             startTimeInterpolation = datetime.datetime.now()
-            print "<----> Interpolation Main Interpolation for %s begin\n"%(columnName)
+            print "<----> Interpolation Main Interpolation for %s begin\n" % (columnName)
             # (R_TimeIndex(1):R_TimeIndex(end)+RR_interval(end),1);
             # Orig_Signal         = Denosied_Signal
             Orig_Signal = Denosied_Signal.tolist()
@@ -259,77 +255,44 @@ if __name__ == "__main__":
 
             RR_interval_count = len(RR_interval)
 
-            ## need to transfer to spark
-            print"<----> Interpolation Main broadcast MaxLength\n"
-            broadCast_MaxLength = sc.broadcast(MaxLength)
-            #print "<----> Interpolation Main broadcast R_TimeIndex\n"
-            #broadCast_R_TimeIndex = sc.broadcast(R_TimeIndex)
-            print "<----> Interpolation Main broadcast RR_interval\n"
-            broadCast_RR_interval = sc.broadcast(RR_interval)
-            print "<----> Interpolation Main broadcast Orig_Signal\n"
-            broadCast_Orig_Signal = sc.broadcast(Orig_Signal)
-            print "<----> Interpolation Main broadcast Orig_Time\n"
-            broadCast_Orig_Time = sc.broadcast(Orig_Time)
-
             print "<----> Interpolation Main parallelize Interpolation_RR_Interval\n"
-            rdd_IntropRR_interval = sc.parallelize(range(RR_interval_count)).map(Interpolation_RR_Interval)
-            IntropRR_interval     = rdd_IntropRR_interval.collect()
+            rdd_IntropRR_interval = sc.parallelize(range(RR_interval_count)).map(lambda i: MaxLength)
+            IntropRR_interval = rdd_IntropRR_interval.collect()
             rdd_IntropRR_interval.unpersist()
 
+
             print "<----> Interpolation Main parallelize Interpolation_TimeIndex\n"
-            rdd_IntropR_TimeIndex = sc.parallelize(range(RR_interval_count)).map(lambda i: R_TimeIndex[0] + MaxLength*i)
-            IntropR_TimeIndex     = rdd_IntropR_TimeIndex.collect()
+            rdd_IntropR_TimeIndex = sc.parallelize(range(RR_interval_count)).map(lambda i: R_TimeIndex[0] + MaxLength * i)
+            IntropR_TimeIndex = rdd_IntropR_TimeIndex.collect()
             rdd_IntropR_TimeIndex.unpersist()
 
             f1 = interp1d(Orig_Time, Orig_Signal)
 
             startTime = Orig_Time[0]
-            endTime = Orig_Time[len(Orig_Time)-1]
-            step = (endTime - startTime)/(MaxLength*RR_interval_count)
+            endTime = Orig_Time[len(Orig_Time) - 1]
+            step = (endTime - startTime) / (MaxLength * RR_interval_count)
 
             print "<----> Interpolation main parallelize Interpolation_Time\n"
-            #Introp_Time = floatrange(startTime, endTime, MaxLength * RR_interval_count)
-            rdd_Introp_Time = sc.parallelize(range(MaxLength * RR_interval_count)).map(lambda i: startTime + float(i)*step)
-            Introp_Time = rdd_Introp_Time.collect()
+            rdd_Introp_Time = sc.parallelize(range(MaxLength * RR_interval_count)).map(lambda i: Row(i, startTime + float(i) * step))
+
+            Introp_Time = rdd_Introp_Time.map(lambda row: row[1]).collect()
+
             rdd_Introp_Time.unpersist()
-
-
 
             print "<----> Interpolation Main parallelize Interpolation_Signal\n"
             Introp_Signal = list(f1(Introp_Time))
-            #broadcast_f1 = sc.broadcast(f1)
-            #rdd_Introp_Signal = sc.parallelize(Introp_Time).map(broadcast_f1.value)
-            #Introp_Signal     = rdd_Introp_Signal.collect()
-            #rdd_Introp_Signal.unpersist()
-            #broadcast_f1.unpersist()
-
-
-
-
-
-            #plt.plot(Orig_Time, Orig_Signal, "o", Introp_Time, Introp_Signal, '-')
-
-            #plt.show()
-
-
-
-
 
 
             endTimeInterpolation = datetime.datetime.now()
-            print "<----> Interpolation Main Interpolation for %s end, execute time %d s\n"%(columnName, (endTimeInterpolation - startTimeInterpolation).seconds)
+            print "<----> Interpolation Main Interpolation for %s end, execute time %d s\n" % (columnName, (endTimeInterpolation - startTimeInterpolation).seconds)
 
-            broadCast_MaxLength.unpersist()
-            #broadCast_R_TimeIndex.unpersist()
-            broadCast_RR_interval.unpersist()
-            broadCast_Orig_Signal.unpersist()
-            broadCast_Orig_Time.unpersist()
-            # Interpolation end
+            ## Interpolation end
 
             # Check BeatType
             print "<----> Interpolation Main Check BeatType begin\n"
             startTimeBeatType = datetime.datetime.now()
-            BeatType = ['N', 'L', 'R', 'B', 'A', 'a', 'J', 'S', 'V', 'r', 'F', 'e', 'j', 'n', 'E', '/', 'f', 'Q','?']  # remove'|'
+            BeatType = ['N', 'L', 'R', 'B', 'A', 'a', 'J', 'S', 'V', 'r', 'F', 'e', 'j', 'n', 'E', '/', 'f', 'Q',
+                        '?']  # remove'|'
             # BeatIndex       = 1
             BeatIndex = 0
             # BeatTypeArray   = type(1)
@@ -343,7 +306,8 @@ if __name__ == "__main__":
                     BeatTypeArray.append(type[i])
                     BeatTimeArray.append(ann[i])
             endTimeBeatType = datetime.datetime.now()
-            print "<----> Interpolation Main Check BeatType end, execute time %ds\n"%((endTimeBeatType-startTimeBeatType).seconds)
+            print "<----> Interpolation Main Check BeatType end, execute time %ds\n" % (
+                (endTimeBeatType - startTimeBeatType).seconds)
             # Check BeatType end
 
             # Log to file
@@ -375,88 +339,71 @@ if __name__ == "__main__":
                     if ALL_GroupFiles[icounter] in MLII_2_GroupFiles:
                         leadstr = '_MLII_'
 
-                # OutPutFileName = PATH + '/Interpolation/' + str(ALL_GroupFiles[icounter]) + leadstr + str(MaxLength) + '.txt')
-                #OutPutFileName = '/home/terence01/workspace/Spark/Interpolatation/'+str(ALL_GroupFiles[icounter]) + leadstr + str(MaxLength) + '.txt'
-                OutPutFileName = OutPutPath + str(ALL_GroupFiles[icounter]) + leadstr + str(MaxLength)
-                # OutPutFileName = strcat(PATH,'/Interpolation/',num2str(ALL_GroupFiles(icounter)),leadstr,'_RR_',num2str(MaxLength),'.txt');
-                # OutPutFileName2 = '/home/terence01/workspace/Spark/Interpolatation/'+str(ALL_GroupFiles[icounter]) + leadstr + '_RR_' + str(MaxLength) + '.txt'
-                OutPutFileName2 = OutPutPath + str(ALL_GroupFiles[icounter]) + leadstr + '_RR_' + str(MaxLength)
+
+                OutPutFileName = '/Interpolation/' + str(ALL_GroupFiles[icounter]) + leadstr + str(MaxLength) + '.csv'
+                OutPutFileName2 = '/Interpolation/' + str(ALL_GroupFiles[icounter]) + leadstr + '_RR_' + str(MaxLength) + '.csv'
 
                 LogFileOption1 = True
-                LogFileOption2 = False
 
                 startTimeLogFile = datetime.datetime.now()
-
 
                 if LogFileOption1 == True:
                     print "<----> Interpolation Main Log file %s\n" % (OutPutFileName)
                     print "<----> Interpolation Main Log file %s, step 1\n" % (OutPutFileName)
-                    temp_df = pd.DataFrame({"Time":Introp_Time, "Voltage":Introp_Signal})
+                    temp_df = pd.DataFrame({"Time": Introp_Time, "Voltage": Introp_Signal})
+
                     print "<----> Interpolation Main Log file %s, step 2\n" % (OutPutFileName)
-                    sparkDF = SQLContext(sc).createDataFrame(temp_df)
+                    csv_content = temp_df.to_csv(index = False)
+
                     print "<----> Interpolation Main Log file %s, step 3\n" % (OutPutFileName)
-                    sparkDF.write.save(OutPutFileName)
-                    print "<----> Interpolation Main Log file %s, step 4\n" % (OutPutFileName)
-                    sparkDF.unpersist()
-                    del temp_df
-                    del sparkDF
+                    try:
+                        client.write(OutPutFileName, csv_content, overwrite=True)
+                    finally:
+                        print "<----> Interpolation Main Log file %s, step 4\n" % (OutPutFileName)
+                        del temp_df
 
-                    print "<----> Interpolation Main Log file %s\n"%(OutPutFileName2)
-                    ## need to transfer to spark
-
+                    print "<----> Interpolation Main Log file %s\n" % (OutPutFileName2)
                     TimeIndex_Len = len(IntropR_TimeIndex)
                     IntropRR_interval_Len = len(IntropRR_interval)
                     BeatType_Len = len(BeatTypeArray)
 
-                    if((TimeIndex_Len != IntropRR_interval_Len ) or (TimeIndex_Len != BeatType_Len) or (IntropRR_interval_Len != BeatType_Len)):
+                    if ((TimeIndex_Len != IntropRR_interval_Len) or (TimeIndex_Len != BeatType_Len) or (
+                                IntropRR_interval_Len != BeatType_Len)):
                         print "<---->Error Interpolation Main Log file %s\n" % (OutPutFileName2)
                         continue
 
                     print "<----> Interpolation Main Log file %s, step 1\n" % (OutPutFileName2)
-                    temp_df2 = pd.DataFrame({"Start_Time":IntropR_TimeIndex, "RR_Interval":IntropRR_interval, "BeatType":BeatTypeArray})
+                    temp_df2 = pd.DataFrame({"Start_Time": IntropR_TimeIndex, "RR_Interval": IntropRR_interval, "BeatType": BeatTypeArray})
+
                     print "<----> Interpolation Main Log file %s, step 2\n" % (OutPutFileName2)
-                    sparkDF2 = SQLContext(sc).createDataFrame(temp_df2)
+                    csv_content2 = temp_df2.to_csv(index = False)
+
                     print "<----> Interpolation Main Log file %s, step 3\n" % (OutPutFileName2)
-                    sparkDF2.write.save(OutPutFileName2)
-                    print "<----> Interpolation Main Log file %s, step 4\n" % (OutPutFileName2)
-                    sparkDF2.unpersist()
-                    del temp_df2
-                    del sparkDF2
-
-                elif LogFileOption2 == True:
-                    print "<----> Interpolation Main Log file %s\n" % (OutPutFileName)
-                    broadcast_Introp_Signal = sc.broadcast(Introp_Signal)
-                    broadcast_Introp_Time = sc.broadcast(Introp_Time)
-
-                    rdd_tempOutPutFile = sc.parallelize(range(0, len(Introp_Time))).map(lambda n: '%0.4f \t %0.4f \t\n' % (broadcast_Introp_Time.value[n], broadcast_Introp_Signal.value[n]))
-                    rdd_tempOutPutFile.saveAsTextFile(OutPutFileName)
-
-                    rdd_tempOutPutFile.unpersist()
-                    broadcast_Introp_Signal.unpersist()
-                    broadcast_Introp_Time.unpersist()
-
-                    print "<----> Interpolation Main Log file %s\n" % (OutPutFileName2)
-                    broadcast_IntropR_TimeIndex = sc.broadcast(IntropR_TimeIndex)
-                    broadcast_IntropRR_interval = sc.broadcast(IntropRR_interval)
-                    broadcast_BeatTypeArray = sc.broadcast(BeatTypeArray)
-
-                    rdd_tempOutPutFile2 = sc.parallelize(range(0, len(BeatTypeArray))).map(lambda n: '%s\t %s\t %s\t\n ' % (broadcast_IntropR_TimeIndex.value[n], broadcast_IntropRR_interval.value[n], broadcast_BeatTypeArray.value[n]))
-                    rdd_tempOutPutFile2.saveAsTextFile(OutPutFileName2)
-
-                    rdd_tempOutPutFile2.unpersist()
-                    broadcast_IntropR_TimeIndex.unpersist()
-                    broadcast_IntropRR_interval.unpersist()
-                    broadcast_BeatTypeArray.unpersist()
+                    try:
+                        client.write(OutPutFileName2, csv_content2, overwrite=True)
+                    finally:
+                        print "<----> Interpolation Main Log file %s, step 4\n" % (OutPutFileName2)
+                        del temp_df2
 
                 endTimeLogFile = datetime.datetime.now()
-
                 print "<----> Interpolation Main Log file for %s end, execute time %ds\n" % (columnName, (endTimeLogFile - startTimeLogFile).seconds)
-            endTimeProcessing = datetime.datetime.now()
-            print "<----> Interpolation Main Processing %s end, execute time %ds\n" % (columnName, (endTimeProcessing-startTimeProcessing).seconds)
-                # Log to file end
-        endTimeEachFile = datetime.datetime.now()
-        print "<----> Interpolation Main Processing %s end, execute time %ds\n" % (VoltageFile, (endTimeEachFile-startTimeEachFile).seconds)
-    endTimeMain = datetime.datetime.now()
 
-    print "<----> Interpolation Main end, execute time %ds\n"%((endTimeMain-startTimeMain).seconds)
+            endTimeProcessing = datetime.datetime.now()
+            print "<----> Interpolation Main Processing %s end, execute time %ds\n" % (columnName, (endTimeProcessing - startTimeProcessing).seconds)
+            # Log to file end
+
+        endTimeEachFile = datetime.datetime.now()
+        print "<----> Interpolation Main Processing %s end, execute time %ds\n" % (VoltageFile, (endTimeEachFile - startTimeEachFile).seconds)
+
+    endTimeMain = datetime.datetime.now()
+    print "<----> Interpolation Main end, execute time %ds\n" % ((endTimeMain - startTimeMain).seconds)
+
+# End of 'Interpolation'
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName('Interpolation')
+    conf.set("spark.executor.heartbeatInterval", "60s")
+    sc = SparkContext(conf = conf)
+
+    Interpolation()
 # End of 'main'
